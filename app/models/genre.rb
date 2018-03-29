5d677e92aed45152758f6847751cd9a86f14fffd
Antonio Reid <read.musik@gmail.com>
2018-03-29 00:12:24 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,11 +3,12 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    songs.count 
+    songs.count
     # return the number of songs in a genre
   end
 
   def artist_count
+    artists.count 
     # return the number of artists associated with the genre
   end
 
